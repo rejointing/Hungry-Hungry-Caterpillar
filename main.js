8b77dbee86367fe3c58d9fe4f2aaad77cc44ea7b
REJOINT <brecab120@gmail.com>
2023-01-22 13:30:28 -0500

    function Game

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
-ctx.fillRect(0,0,400,400);
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+let speed = 7;
+
+// Game loop:
 
 function drawGame(){
-    ctx.fillStyle = "White";
-    ctx.fillStyle();
-    
+    console.log('draw game');
+    setTimeout(drawGame, 1000/ speed);
 }
+
+drawGame();
+
+// audio
+// collision detection
+// keyboard input
+// adding text
